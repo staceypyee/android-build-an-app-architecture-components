37678bdbf5336ca86ccbc7d95390a8896333dd22
Stacey <staceywongdev@gmail.com>
2018-09-26 16:11:32 +0800

    Added SunshineDatabase class which defines the database(entities etc) for the app - Applies Singleton pattern - Added DAOs

---
+++ app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
@@ -0,0 +1,31 @@
+package com.example.android.sunshine.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+// @params entities: is a Class[]
+@Database(entities = {WeatherEntry.class}, version = 1)
+public abstract class SunshineDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "weather"; // weather is a singular?
+
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static volatile SunshineDatabase sInstance;
+
+    public static SunshineDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            SunshineDatabase.class, SunshineDatabase.DATABASE_NAME).build();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    // Associated DAOs for the database: Where the other classes use
+    public abstract WeatherDao weatherDao();
+}
