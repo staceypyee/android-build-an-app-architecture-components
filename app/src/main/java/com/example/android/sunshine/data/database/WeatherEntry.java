57aaf4c8c011e2306802e09d2c445c5919255607
Stacey <staceywongdev@gmail.com>
2018-09-26 15:10:33 +0800

    Make WeatherEntry as an Entity

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
@@ -16,10 +16,23 @@
 
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+/* Defines the schema of a table in {@link Room} for a single weather
+ * forecast. The date is used as an {@link Index} so that its uniqueness can be ensured. Indexes
+ * also allow for fast lookup for the column.
+ */
+
+// date value is always unique
+@Entity(tableName = "weather", indices = {@Index(value = {"date"}, unique = true)})
 public class WeatherEntry {
 
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private int weatherIconId;
     private Date date;
@@ -33,16 +46,31 @@ public class WeatherEntry {
     /**
      * This constructor is used by OpenWeatherJsonParser. When the network fetch has JSON data, it
      * converts this data to WeatherEntry objects using this constructor.
+     *
      * @param weatherIconId Image id for weather
-     * @param date Date of weather
-     * @param min Min temperature
-     * @param max Max temperature
-     * @param humidity Humidity for the day
-     * @param pressure Barometric pressure
-     * @param wind Wind speed
-     * @param degrees Wind direction
+     * @param date          Date of weather
+     * @param min           Min temperature
+     * @param max           Max temperature
+     * @param humidity      Humidity for the day
+     * @param pressure      Barometric pressure
+     * @param wind          Wind speed
+     * @param degrees       Wind direction
      */
+    @Ignore// Not used by Room
     public WeatherEntry(int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.weatherIconId = weatherIconId;
+        this.date = date;
+        this.min = min;
+        this.max = max;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.degrees = degrees;
+    }
+
+    // Constructor used by Room to create WeatherEntries
+    public WeatherEntry(int id, int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.id = id;
         this.weatherIconId = weatherIconId;
         this.date = date;
         this.min = min;
